18e35245eb67feae8c1e7b43774a7a8f6e968508
pete <p.rogers@gold.ac.uk>
2012-07-06 16:24:51 +0100

    added search queries to xml gathering

---
+++ geoNews/src/dataGathering/EMMSearchConstructor.java
@@ -0,0 +1,12 @@
+package dataGathering;
+
+public class EMMSearchConstructor {
+	
+	
+	public String urlPrefix(){
+		
+		String pref = "http://emm.newsbrief.eu/rss?";
+		return pref;
+	}
+
+}
--- geoNews/src/dataGathering/GatherXML.java
+++ geoNews/src/dataGathering/GatherXML.java
@@ -1,6 +1,8 @@
 package dataGathering;
 
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 import processing.xml.XMLElement;
 
@@ -11,34 +13,86 @@ public class GatherXML {
 	 */
 	
 	
-	XMLElement xml;
+	
 	PApplet p;
 	
 	public GatherXML(){
-		p = new PApplet();
-		xml =  new XMLElement(p, "http://emm.newsbrief.eu/rss?type=alert&id=Jamaica&language=en");
-		//System.out.println(xml.getAttributeCount());
 		
-		System.out.println(xml.toString());
-		XMLElement channel = xml.getChild(0);
-		System.out.println(channel.getChild("item"));
+		String tester = "http://emm.newsbrief.eu/rss?type=alert&id=Jamaica&category=AnimalHealth&language=all";
+		tester = "http://emm.newsbrief.eu/rss?type=category&id=UnitedKingdom&language=all&duplicates=false";
+		//tester = "http://emm.newsexplorer.eu/rss?type=clusters&language=en";
+		//tester = "http://emm.newsbrief.eu/rss?type=entity&id=11&language=fr";
+		//tester = "http://emm.newsexplorer.eu/rss?type=entity&id=11&language=fr";
+		XMLElement channel = rawXML(tester);
 		XMLElement[] items = channel.getChildren("item");
 		
-		System.out.println(items.length);
 		for (int i = 0; i < items.length; i ++){
-			//XMLElement description = ;
-			
 			try {
-				System.out.println(items[i].getChild("georss:point").getContent());
+
+				//System.out.println(detectPlaceNameInXML("jamaica", items[i]));
+				if (detectPlaceNameInXML("london", items[i]) == true){
+					System.out.println(items[i].getChild("description").getContent());
+					System.out.println(items[i].getChild("georss:point").getContent());
+					entityList(items[i]);
+					
+				}
+				
+			
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
-		}
-		 
+		} 
 	}
+	
+	public XMLElement rawXML(String url){
+		p = new PApplet();
+		XMLElement xml =  new XMLElement(p, url);
+		XMLElement channel = xml.getChild(0);
+		return channel;
+	}
+	
+	
+	public ArrayList entityList(XMLElement xml){
+		ArrayList list = new ArrayList();
+		XMLElement[] items = xml.getChildren("emm:entity");
+		for (int i = 0; i < items.length; i ++){
+			System.out.print(items[i].getContent()+" ");
+			System.out.println(items[i].getString("id"));
+		}
+		return list;
+		
+		
+	}
+	
+	public boolean detectPlaceNameInXML(String name, XMLElement xml){
+			String a = "";
+			String b = "";
+			String c = "";
+			try {
+				a = xml.getChild("description").getContent();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				b = xml.getChild("title").getContent();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				c = xml.getChild("link").getContent();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String checker = a+b+c;
+			checker = checker.toLowerCase();
+			return checker.contains(name);
+}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new GatherXML();
